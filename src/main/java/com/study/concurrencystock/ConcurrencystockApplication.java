4f40df3883e2942b14342f1a7df9991a0ff8c555
Brett Ahn <abu89@naver.com>
2024-11-11 23:15:06 +0900

    Initial commit

---
+++ src/main/java/com/study/concurrencystock/ConcurrencystockApplication.java
@@ -0,0 +1,13 @@
+package com.study.concurrencystock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencystockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurrencystockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/concurrencystock/ConcurrencystockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.concurrencystock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrencystockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
